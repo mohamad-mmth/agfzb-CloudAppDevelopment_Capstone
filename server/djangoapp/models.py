f5e62b7961dd63b86a9ea5623e4074547e9298fb
Mohamad <mohamad@mammoth.tv>
2023-05-22 17:04:32 +0100

    week 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=280)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +25,51 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    #constant for c_type choices
+    TYPES = (
+            ("BMW", "BMW"), ("Mercedes", "Mercedes"), ("WAGON", "WAGON"), ("Sedan", "Sedan"), ("SUV", "SUV")
+        )
+
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    c_type = models.CharField(max_length=30, choices=TYPES)
+    dealer_id = models.IntegerField()
+    year = models.DateField()
+
+    def __str__(self):
+        return "Name: " + self.name + \
+                " Make Name: "+ self.make.name + \
+                " Type: " + self.c_type + \
+                " Dealer ID: " + str(self.dealer_id)+ \
+                " Year: " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
